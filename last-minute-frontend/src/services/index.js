5fc5a11e83122c6dc6f93c6089f091259fa5bb58
Koa Garces <koajgarces@yahoo.com>
2022-05-16 05:11:00 -0700

    added to index for deployment

--- last-minute-frontend/src/services/index.js
+++ last-minute-frontend/src/services/index.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const Client = axios.create({ baseURL: "http://localhost:3001" });
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://git.heroku.com/last-minute-backend.git"
+    : "http://localhost:3001";
+
+const Client = axios.create({ baseURL: BASE_URL });
 
 Client.interceptors.request.use(
   (config) => {
